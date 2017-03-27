c5dd496e7c20c6af021749f6c8478c933a4f2e51
Jacob Rosenthal <jakerosenthal@gmail.com>
2017-03-26 23:49:00 -0700

    cli: close before exit

--- cli.js
+++ cli.js
@@ -11,6 +11,7 @@ var transport = new Transport();
 if (process.argv.indexOf('--reset') !== -1) {
 	nmgr.reset(transport, function(err, data){
 		console.log("nmgr.reset", err, data);
+		transport.close();
 		process.exit(0);
 	});
 }
