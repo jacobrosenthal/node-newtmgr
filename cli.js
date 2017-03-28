40b6243d245aff564dcb0dd4e9ddc0fac8db4818
Jacob Rosenthal <jakerosenthal@gmail.com>
2017-03-28 00:18:28 -0700

    dont use from2 inside protocol as it closes stream before read operation

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 var SerialPort = require('serialport');
 var through2 = require('through2');
-// var concat = require('concat-stream');
+var concat = require('concat-stream');
+var from2 = require('from2');
 
 var nmgr = require('./nmgr');
 var serial = require('./serial');
@@ -11,25 +12,19 @@ var stream = new SerialPort('/dev/tty.usbmodem1411', {
 
 if (process.argv.indexOf('--reset') !== -1) {
 
-  var listen = through2.obj(function (chunk, enc, callback) {
-    console.log(chunk)
-    process.exit(0)
-  });
+    var listen = through2.obj(function (chunk, enc, callback) {
+      console.log(chunk)
+      process.exit(0)
+    });
 
-  // var complete = function(data){
-  //   console.log(data)
-  //   process.exit(0)
-  // }
+    // from2 will close transport on you, so only use if you only want to do one operation
+    from2([nmgr.generateResetBuffer()])
+      .pipe(serial.encode())
+      .pipe(stream);
 
-  nmgr.reset()
-  .pipe(serial.encode())
-  .pipe(stream)
-
-  stream
-  .pipe(serial.decode())
-  .pipe(nmgr.decode())
-  .pipe(listen)
-  // .pipe(concat(complete));
-
+    stream
+      .pipe(serial.decode())
+      .pipe(nmgr.decode())
+      .pipe(listen);
 }
 
